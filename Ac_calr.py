d90b45e366721f298d99cd9d3876d31c3a438414
Teodora <teo.vujovic@gmail.com>
2022-11-01 02:06:59 -0400

    Sanity check OASIS I/O code using mindseg

---
+++ Ac_calr.py
@@ -0,0 +1,242 @@
+from dataclasses import dataclass
+from typing import Optional, Tuple
+from pathlib import Path
+from tqdm import tqdm
+from collections import defaultdict
+
+import nibabel as nib
+
+import os
+import json
+import typer
+import einops
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+
+from common import MINDSSC, get_identity_affine_grid #, apply_displacement_field, get_labels
+from metrics import compute_total_registration_error, compute_dice
+from Ab_calr_loops import inst_optimization
+# from instopt_loops_discretesteps_ablation import lr_step_optimization
+# from ioa_swa_loops import swa_optimization
+
+app = typer.Typer()
+
+get_spacing = lambda x: np.sqrt(np.sum(x.affine[:3, :3] * x.affine[:3, :3], axis=0))
+add_bc_dim = lambda x: einops.repeat(x, "d h w -> b c d h w", b=1, c=1).float()
+
+GPU_iden = 1
+torch.cuda.set_device(GPU_iden)
+
+@dataclass
+class InstanceOptData:
+    fixed_image: Path
+    moving_image: Path
+    fixed_mask: Optional[Path]
+    moving_mask: Optional[Path]
+    fixed_segmentation: Optional[Path]
+    moving_segmentation: Optional[Path]
+    fixed_keypoints: Optional[Path]
+    moving_keypoints: Optional[Path]
+    fixed_landmarks: Optional[Path]
+    moving_landmarks: Optional[Path]
+    rnn_disp_path: Optional[Path]
+    disp_name: Optional[str]
+    moving_name: Optional[str]
+
+
+def get_paths(
+        data_json,
+        split_val,
+        disp_root,
+):
+    with open(data_json, "r") as f:
+        data = json.load(f)[split_val]
+
+    has_segs = "fixed_segmentation" in data[0]
+    has_kps = "fixed_keypoints" in data[0]
+    has_mask = "fixed_mask" in data[0]
+    has_lms = "fixed_landmarks" in data[0]
+
+    for v in data:
+        img_number = v['moving_image'][-16:-12]
+        fix_number = v['fixed_image'][-16:-12]
+        moving_name = f"disp_{img_number}_0001.nii.gz" # NLST specific
+        disp_name = f"disp_{fix_number}_{img_number}.nii.gz"
+        disp_path = disp_root / disp_name
+
+        yield InstanceOptData(
+            fixed_image=Path(v["fixed_image"]),
+            moving_image=Path(v["moving_image"]),
+            fixed_mask=Path(v["fixed_mask"]) if has_mask else None,
+            moving_mask=Path(v["moving_mask"]) if has_mask else None,
+            fixed_segmentation=Path(v["fixed_segmentation"]) if has_segs else None,
+            moving_segmentation=Path(v["moving_segmentation"]) if has_segs else None,
+            fixed_keypoints=Path(v["fixed_keypoints"]) if has_kps else None,
+            moving_keypoints=Path(v["moving_keypoints"]) if has_kps else None,
+            fixed_landmarks=Path(v["fixed_landmarks"]) if has_lms else None,
+            moving_landmarks=Path(v["fixed_landmarks"]) if has_lms else None,
+            rnn_disp_path=Path(disp_path),
+            disp_name=disp_name,
+            moving_name=moving_name,
+        )
+
+
+def apply_instance_optimization(
+        data_json: Path = Path("./pth_0929/NLST.json"),
+        initial_disp_root: Path = Path("./eval_0929"),
+        save_directory: Path = Path("././CVIS/41b_calr"),
+        img_shape = (224,192,224),
+        split_val: str = "train",
+        half_res: bool = False,
+        use_mask: bool = True,
+):
+
+    # img_shape = (160,224,192)
+    save_directory.mkdir(exist_ok=True)
+    (save_directory / "disps").mkdir(exist_ok=True)
+    device = "cuda"
+
+    checkpoint_dir = save_directory / 'opt_checkpoints'
+    checkpoint_dir.mkdir(exist_ok=True)
+    measurements = defaultdict(dict)
+
+    H, W, D = img_shape
+    grid0 = get_identity_affine_grid((H//2, W //2, D //2))
+
+    gen = tqdm(get_paths(data_json=data_json, split_val=split_val, disp_root=initial_disp_root))
+
+    for data in gen:
+        disp_noise = torch.tensor(np.random.random((1, 3, H // 2, W // 2, D // 2))).cuda()
+        image_name = data.fixed_image.name[-16:-12]
+
+        fixed_nib = nib.load(data.fixed_image)
+        moving_nib = nib.load(data.moving_image)
+
+        fixed = add_bc_dim(torch.from_numpy(fixed_nib.get_fdata())).to(device).squeeze()
+        moving = add_bc_dim(torch.from_numpy(moving_nib.get_fdata())).to(device).squeeze()
+
+        if data.fixed_keypoints is not None:
+            fixed_keypoints = np.loadtxt(data.fixed_keypoints, delimiter=",")
+            moving_keypoints = np.loadtxt(data.moving_keypoints, delimiter=",")
+            fs = get_spacing(fixed_nib)
+
+        if data.fixed_mask is not None:
+            fixed_mask = torch.from_numpy(nib.load(data.fixed_mask).get_fdata().astype('float32')).to(device)
+            moving_mask = torch.from_numpy(nib.load(data.moving_mask).get_fdata().astype('float32')).to(device)
+
+        if use_mask and (data.fixed_mask is not None):
+            fixed = fixed_mask * fixed
+            moving = moving_mask * moving
+
+        if data.fixed_segmentation is not None:
+            fixed_seg = (nib.load(data.fixed_segmentation)).get_fdata().astype("float")
+            moving_seg = (nib.load(data.moving_segmentation)).get_fdata().astype("float")
+
+            # label_list = get_labels(torch.tensor(fixed_seg), torch.tensor(moving_seg))
+
+            fixed_segt = torch.tensor(fixed_seg).to(device).unsqueeze(0).unsqueeze(0)
+            moving_segt = torch.tensor(moving_seg).to(device).unsqueeze(0).unsqueeze(0)
+
+        fixed = (fixed - fixed.min())/(fixed.max() - fixed.min())
+        moving = (moving - moving.min())/(moving.max() - moving.min())
+
+        mindssc_fix_ = MINDSSC(fixed.unsqueeze(0).unsqueeze(0), 1, 2).half()
+        mindssc_mov_ = MINDSSC(moving.unsqueeze(0).unsqueeze(0), 1, 2).half()
+
+        grid_sp = 2
+        with torch.no_grad():
+            mind_fix_ = F.avg_pool3d(mindssc_fix_, grid_sp, stride=grid_sp)
+            mind_mov_ = F.avg_pool3d(mindssc_mov_, grid_sp, stride=grid_sp)
+
+        if half_res and data.fixed_segmentation is not None:
+            fixed_segt = F.avg_pool3d(fixed_segt, grid_sp, stride=grid_sp)
+            moving_segt = F.avg_pool3d(moving_segt, grid_sp, stride=grid_sp)
+
+        net = inst_optimization(
+            disp=disp_noise,
+            mind_fixed=mind_fix_,
+            mind_moving=mind_mov_,
+            lambda_weight=1.25,
+            image_shape=tuple(s//grid_sp for s in img_shape),
+            img_name=image_name, norm=grid_sp,
+            fkp=torch.tensor(fixed_keypoints) if data.fixed_keypoints is not None else None,
+            mkp=torch.tensor(moving_keypoints) if data.fixed_keypoints is not None else None,
+            fs=torch.tensor(fs) if data.fixed_keypoints is not None else None,
+            checkpoint_dir=checkpoint_dir, grid0=grid0,
+            fsegt=fixed_segt if data.fixed_segmentation is not None and half_res else None,
+            msegt=moving_segt if data.fixed_segmentation is not None and half_res else None,
+        )
+
+        disp_sample = F.avg_pool3d(
+            F.avg_pool3d(net[0].weight, 3, stride=1, padding=1), 3, stride=1, padding=1
+        ).permute(0, 2, 3, 4, 1)
+
+        scale = (torch.tensor([(H - 1) / 2, (W - 1) / 2, (D - 1) / 2, ]).cuda().unsqueeze(0))
+        grid_disp = (grid0.view(-1, 3).cuda().float()
+                     + ((disp_sample.view(-1, 3)) / scale).flip(1).float())
+
+        patch_mov_sampled = F.grid_sample(
+            mind_mov_.float(),
+            grid_disp.view(1, (H // 2), (W // 2), (D // 2), 3).cuda(),
+            align_corners=False,
+            mode="bilinear",
+            padding_mode="border")
+
+        sampled_cost = (patch_mov_sampled - mind_fix_).pow(2).mean(1) * 12
+        loss = sampled_cost.mean().item()
+        measurements[data.disp_name]["img_loss"] = loss
+
+        fitted_grid = disp_sample.permute(0, 4, 1, 2, 3).detach()
+        disp_hr = F.interpolate(
+            fitted_grid * grid_sp,
+            size=fixed_nib.shape,
+            mode="trilinear",
+            align_corners=False,
+        )
+
+        disp_np = disp_hr.squeeze(0).detach().cpu().numpy()
+
+        l2r_disp = einops.rearrange(disp_np.squeeze(), 't h w d -> h w d t')
+        new_disp_path = save_directory / "disps" / data.disp_name
+        new_disp_path.parent.mkdir(exist_ok=True)
+
+        # if data.fixed_segmentation is not None:
+        #     moved_seg = apply_displacement_field(disp_np, moving_seg, order=0)
+        #
+        #     dice = compute_dice(
+        #         fixed_seg, moving_seg, moved_seg, labels=label_list #type: ignore
+        #     )
+        #     measurements[data.disp_name]["dice"] = dice
+        #     print(dice)
+
+        if data.fixed_keypoints is not None:
+
+            tre_np = compute_total_registration_error(
+                fixed_keypoints, moving_keypoints, l2r_disp, fs, fs
+            )
+
+            fixed_keypoints = torch.from_numpy(fixed_keypoints)
+            moving_keypoints = torch.from_numpy(moving_keypoints)
+
+            measurements[data.disp_name]["tre"] = tre_np
+            print(tre_np)
+
+        # displacement_nib = nib.Nifti1Image(l2r_disp, affine=moving_nib.affine)
+        # nib.save(displacement_nib, new_disp_path)
+        #
+        # moved_mask = apply_displacement_field(disp_np, nib.load(data.moving_mask).get_fdata())
+        # mask_nib = nib.Nifti1Image(moved_mask, affine=fixed_nib.affine)
+        # nib.save(mask_nib, save_directory / "masks" / data.moving_name)
+        #
+        # moved_image = apply_displacement_field(disp_np, moving_nib.get_fdata())
+        # moved_nib = nib.Nifti1Image(moved_image, affine=fixed_nib.affine)
+        # nib.save(moved_nib, save_directory / "images" / (data.moving_name))
+
+    with open(save_directory / "measurements.json", "w") as f:
+        json.dump(measurements, f)
+
+
+apply_instance_optimization()
+
---
+++ io_adam_loops.py
@@ -0,0 +1,184 @@
+import numpy as np
+import torch
+import einops
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR
+from torch.utils.tensorboard.writer import SummaryWriter
+from pathlib import Path
+from typing import Dict
+
+from monai.losses.dice import DiceLoss as monaiDice
+from torch.nn import BCELoss
+
+from differentiable_metrics import TotalRegistrationLoss, DiceLoss
+
+
+def tb_optimizer(
+        writer: SummaryWriter,
+        losses_dict: Dict[str, torch.Tensor],
+        step: int,
+) -> None:
+    for loss_name, loss in losses_dict.items():
+        writer.add_scalar(loss_name, loss, global_step=step)
+
+
+trl = TotalRegistrationLoss()
+calc_dice = DiceLoss()
+bce_loss = BCELoss(reduction="mean")
+monaidice = monaiDice(include_background=False)
+
+def caLR_loop(H, W, D,
+             net, grid0, optimizer,
+             mind_fixed, mind_moving,
+             lambda_weight,
+             iterations,
+             schedulera,
+             schedulerb,
+             writer, img_name, fixed_keypoints, moving_keypoints,
+             fixed_spacing, moving_spacing,
+            fsegt, msegt, labels, fullres=False):
+
+    for _ in iterations:
+        iteration_losses = {}
+
+        optimizer.zero_grad()
+
+        disp_sample = F.avg_pool3d(F.avg_pool3d(net[0].weight, 3, stride=1, padding=1), 3, stride=1, padding=1)\
+            .permute(0, 2, 3, 4, 1)
+
+        reg_loss = (
+                lambda_weight
+                * ((disp_sample[0, :, 1:, :] - disp_sample[0, :, :-1, :]) ** 2).mean()
+                + lambda_weight
+                * ((disp_sample[0, 1:, :, :] - disp_sample[0, :-1, :, :]) ** 2).mean()
+                + lambda_weight
+                * ((disp_sample[0, :, :, 1:] - disp_sample[0, :, :, :-1]) ** 2).mean())
+
+        if fsegt is not None and msegt is not None:
+            dice = calc_dice(fsegt, msegt, disp_sample.permute(0, 4, 1, 2, 3))
+            # dice.retain_grad()
+            iteration_losses["dice"] = dice.item()
+            print(dice.item())
+        #
+        #     # wsegt = warp_image(disp_sample.permute(0, 4, 1, 2, 3), msegt, mode="nearest")
+        #     # bce = bce_loss(fsegt, wsegt)
+        #     # bce.retain_grad()
+        #     # iteration_losses["bce"] = bce.item()
+        #     # print(bce.item())
+
+        if (int(img_name) < 101) and (fixed_keypoints is not None):
+            fitted_grid = disp_sample.permute(0, 4, 1, 2, 3)
+
+            disp_hr = F.interpolate(
+                fitted_grid * 2,
+                size=(H * 2, W * 2, D * 2),
+                mode="trilinear",
+                align_corners=False,
+            )
+            disp_hr.retain_grad()
+
+            disp_sample_full = einops.rearrange(disp_hr, "b c d h w -> (b c) d h w")
+
+            tre = trl(
+                fixed_keypoints,
+                moving_keypoints,
+                disp_sample_full.unsqueeze(0),
+                fixed_spacing,
+                moving_spacing
+            )
+            print(tre.item())
+            iteration_losses["tre"] = tre.item()
+
+        scale = (torch.tensor([(H - 1) / 2, (W - 1) / 2, (D - 1) / 2, ]).cuda().unsqueeze(0))
+        grid_disp = (grid0.view(-1, 3).cuda().float()
+                     + ((disp_sample.view(-1, 3)) / scale).flip(1).float())
+
+        patch_mov_sampled = F.grid_sample(
+            mind_moving.float(),
+            grid_disp.view(1, H, W, D, 3).cuda(),
+            align_corners=False,
+            mode="bilinear",
+            padding_mode="border")
+
+        sampled_cost = (patch_mov_sampled - mind_fixed).pow(2).mean(1) * 12
+        loss = sampled_cost.mean()
+
+        total_loss = loss + reg_loss
+
+        iteration_losses["loss"] = loss.item()
+        iteration_losses["reg_loss"] = reg_loss.item()
+        iteration_losses["total_loss"] = total_loss.item()
+
+        tb_optimizer(writer=writer, losses_dict=iteration_losses, step=_)
+        total_loss.backward(retain_graph=True)
+
+        optimizer.step()
+
+        # if _ > 70:
+        #     if 180<_<250:
+        #         schedulerb.step()
+        #     else:
+        #         schedulera.step()
+
+def inst_optimization(
+        disp,
+        mind_fixed,
+        mind_moving,
+        lambda_weight,
+        image_shape,
+        checkpoint_dir,
+        img_name,
+        norm=None, fkp=None, mkp=None, fs=None, grid0=None,
+        fsegt=None, msegt=None, labels=None,
+):
+    H, W, D = image_shape
+
+    checkpoint_dir = Path(checkpoint_dir / Path(img_name))
+    checkpoint_dir.mkdir(exist_ok=True)
+    writer = SummaryWriter(log_dir=str(checkpoint_dir))
+
+    net = nn.Sequential(nn.Conv3d(3,1,(H, W, D)))
+    net.cuda()
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=1)
+
+    # schedulera = CosineAnnealingLR(optimizer, T_max=200, eta_min=0.1)
+    # schedulerb = LinearLR(optimizer, start_factor=0.999, end_factor=0.1, total_iters=70)
+
+    if fsegt is not None:
+        labels = fsegt.unique()
+
+        fsegs, msegs = [], []
+
+        for i in labels:
+            if i == 0:
+            # if not (i.item() ==1):
+                # if i.item() in [0, 16, 18, 32, 34]:
+                continue
+            fseginter, mseginter = (fsegt == i).float(), (msegt == i).float()
+            fsegs.append(fseginter)
+            msegs.append(mseginter)
+        assert len(fsegs) != 0, "No labels found!"
+
+        fseg = torch.cat(fsegs, dim=1)
+        mseg = torch.cat(msegs, dim=1)
+
+    # fseg = (fsegt==1).float()
+    # mseg = (msegt==1).float()
+
+    caLR_loop(H, W, D,
+             net, grid0, optimizer,
+             mind_fixed, mind_moving,
+             lambda_weight,
+             iterations=range(100),
+             schedulera=None, schedulerb=None,
+             writer=writer, img_name=img_name,
+             fixed_keypoints=fkp,
+             moving_keypoints=mkp,
+             fixed_spacing=fs, moving_spacing=fs,
+             fsegt=fseg if fsegt is not None else None,
+             msegt=mseg if fsegt is not None else None, labels=None)
+
+    return net
